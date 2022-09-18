3df7d160190655d71d563988c3f1f48958ad51bc
Parisa Sohrabi <parisahs@gmail.com>
2022-09-18 18:33:40 -0400

    fetch data from pokemon api by use of useEffect

--- pokemon-api/src/App.js
+++ pokemon-api/src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPokemons(data.results);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <div className="App">
-
+      {pokemons.map((pokemon, i) => (
+        <li key={i}>{pokemon.name}</li>
+      ))}
     </div>
   );
 }
