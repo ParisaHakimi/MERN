2c37bf2995c9163a1af9314ddb85dc74c707896e
Parisa Sohrabi <parisahs@gmail.com>
2022-10-17 14:43:49 -0400

    chaining method

--- user-oop/user.js
+++ user-oop/user.js
@@ -6,20 +6,23 @@ class User {
   }
   makeDeposite(amount) {
     this.accountBalance += amount;
+    return this;
   }
   makeWithdrawal(amount) {
     this.accountBalance -= amount;
+    return this;
   }
-  displauBalance() {
-    this.username, this.accountBalance;
+  displayBalance() {
+    console.log(`name:${this.name}, Account Balance:${this.accountBalance}`);
+    return this;
   }
 }
 const guido = new User("Guido Vann", "guido@email.com");
-guido.makeDeposite(200);
-guido.makeDeposite(450);
+// guido.makeDeposite(200);
+// guido.makeDeposite(450);
 // console.log(guido.accountBalance);
-guido.makeWithdrawal(30);
+// guido.makeWithdrawal(30);
 // console.log(guido.makeWithdrawal);
-guido.displauBalance()
-console.log(guido.displauBalance);
-
+// guido.displayBalance();
+// console.log(guido.displayBalance);
+guido.makeDeposite(200).makeDeposite(50).makeWithdrawal(60).displayBalance();
